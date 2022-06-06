fcfd402512fcae09bc394ffe564571e83d123150
kesel <a.kiselyoff@gmail.com>
2022-06-06 23:41:34 +0300

    step #1

--- src/components/App.js
+++ src/components/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { nanoid } from 'nanoid';
 
 class App extends Component {
   state = {
@@ -6,17 +7,59 @@ class App extends Component {
     name: '',
   };
 
+  handleSubmit = e => {
+    e.preventDefault();
+    const input = e.target[0].value;
+
+    this.setState(prevState => {
+      const newContact = {
+        id: nanoid(),
+        name: input,
+      };
+
+      const newContacts = [...prevState.contacts];
+      newContacts.push(newContact);
+      return { contacts: newContacts };
+    });
+  };
+
   render() {
     return (
-      <>
-        <input
-          type="text"
-          name="name"
-          pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
-          title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
-          required
-        />
-      </>
+      <div
+        style={{
+          height: '100vh',
+          display: 'flex',
+          justifyContent: 'center',
+          alignItems: 'center',
+          flexDirection: 'column',
+        }}
+      >
+        <form action="submit" onSubmit={this.handleSubmit}>
+          <label>
+            Имя
+            <input
+              type="text"
+              name="name"
+              pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
+              title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
+              required
+            />
+          </label>
+          <button type="submit">Сохранить</button>
+        </form>
+        <div>
+          <ul>
+            {this.state.contacts.map(contact => {
+              return (
+                <li>
+                  <p>{contact.name}</p>
+                  <p>{contact.number}</p>
+                </li>
+              );
+            })}
+          </ul>
+        </div>
+      </div>
     );
   }
 }
